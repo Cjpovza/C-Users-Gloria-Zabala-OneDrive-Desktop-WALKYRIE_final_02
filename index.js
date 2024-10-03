a3f73a1ebade7512d434d669ab88fc480ef823a5
carlos <povedac85@gmail.com>
2024-10-02 23:38:20 -0500

    Agregamos el proyecto

---
+++ index.js
@@ -0,0 +1,141 @@
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector('.container-cart-products');
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart');
+});
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-add-cart')) {
+        const product = e.target.parentElement;
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h2').textContent,
+            price: product.querySelector('p').textContent,
+        };
+
+        const exists = allProducts.some(
+            product => product.title === infoProduct.title
+        );
+
+        if (exists) {
+            const products = allProducts.map(product => {
+                if (product.title === infoProduct.title) {
+                    product.quantity++;
+                    return product;
+                } else {
+                    return product;
+                }
+            });
+            allProducts = [...products];
+        } else {
+            allProducts = [...allProducts, infoProduct];
+        }
+
+        showHTML();
+    }
+});
+
+rowProduct.addEventListener('click', e => {
+    if (e.target.classList.contains('icon-close')) {
+        const product = e.target.parentElement;
+        const title = product.querySelector('p').textContent;
+
+        allProducts = allProducts.filter(
+            product => product.title !== title
+        );
+
+        showHTML();
+    }
+});
+
+// Función para mostrar HTML
+const showHTML = () => {
+    if (!allProducts.length) {
+        cartEmpty.classList.remove('hidden');
+        rowProduct.classList.add('hidden');
+        cartTotal.classList.add('hidden');
+    } else {
+        cartEmpty.classList.add('hidden');
+        rowProduct.classList.remove('hidden');
+        cartTotal.classList.remove('hidden');
+    }
+
+    // Limpiar HTML
+    rowProduct.innerHTML = '';
+
+    let total = 0;
+    let totalOfProducts = 0;
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('cart-product');
+
+        containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+        rowProduct.append(containerProduct);
+
+        // Eliminar símbolo de moneda y comas, luego convertir a número
+        const priceNumber = parseFloat(product.price.replace(/[^0-9.-]+/g, ""));
+        total += product.quantity * priceNumber;
+        totalOfProducts += product.quantity;
+    });
+
+    // Llamar a la función para calcular y mostrar el total en pesos colombianos
+    calcularTotalEnCOP(total, totalOfProducts);
+};
+
+// Función para calcular y mostrar el total en pesos colombianos
+function calcularTotalEnCOP(total, totalOfProducts) {
+    const tasaCambioUSD_COP = 1000;
+
+    // Calcular el total en pesos colombianos
+    const totalCOP = total * tasaCambioUSD_COP;
+
+    // Formatear el total a pesos colombianos y mostrar en la interfaz
+    valorTotal.innerText = `$${totalCOP.toLocaleString('es-CO')} COP`;
+    countProducts.innerText = totalOfProducts;
+}
+
+function redirectToHomePage() {
+    // Cambia "index.html" por la ruta correcta de tu página principal
+    window.location.href = 'walkyrie.html';
+}
